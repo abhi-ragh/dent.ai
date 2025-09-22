7be1641e683b0e1e482f5190b68a00736cfc376c
Abhiragh A R <abhiragh0@gmail.com>
2025-09-22 15:29:42 +0530

    Add Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "dentai" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "dent.ai"
+  }
+}
+
